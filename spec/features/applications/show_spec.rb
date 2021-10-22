9911a8608a8746bac043ee356f535dec410df30e
Lesley Sanders <lesleyasanders@gmail.com>
2021-10-22 16:09:03 -0400

    Create view and spec for application show page

---
+++ spec/features/applications/show_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe 'the application show' do
+  it "shows the application and all it's attributes" do
+    application = Application.create(name: 'Larry Sanders', address: '22 Shadowbrook Way Mendham, NJ 07945')
+
+    visit "/applications/#{application.id}"
+
+    expect(page).to have_content(application.name)
+    expect(page).to have_content(application.address)
+  end
+end
