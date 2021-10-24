20f41efe5b8c0dd3bab3b43ad1ed9d7d7541320b
Lesley Sanders <lesleyasanders@gmail.com>
2021-10-24 13:36:39 -0400

    Update applications controller

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -1,3 +1,46 @@
 class ApplicationsController < ApplicationController
+  def index
 
   end
+
+  def show
+    @application = Application.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+    application = Application.new(application_params)
+
+    if application.save
+      redirect_to "/applications/#{application.id}"
+    else
+      redirect_to "/pets/"
+    end
+  end
+
+  def edit
+    @application = Application.find(params[:id])
+  end
+
+  def update
+    application = Application.find(params[:id])
+    if application.update(application_params)
+      redirect_to "/applications/#{application.id}"
+    else
+      redirect_to "/applications/#{application.id}/edit"
+    end
+  end
+
+  def destroy
+    Application.find(params[:id]).destroy
+    redirect_to '/pets'
+  end
+
+  private
+
+  def application_params
+    params.permit(:id, :name, :age, :address, :home_description, :status, :pet_id)
+  end
+end
