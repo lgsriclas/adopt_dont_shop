a17e7d9bb84cf09da0f0b8ef65c707912ba4ede3
Lesley Sanders <lesleyasanders@gmail.com>
2021-10-25 17:17:08 -0400

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+@shelter_1 = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
+@shelter_2 = Shelter.create(name: 'RGV animal shelter', city: 'Harlingen, TX', foster_program: false, rank: 5)
+@shelter_3 = Shelter.create(name: 'Fancy pets of Colorado', city: 'Denver, CO', foster_program: true, rank: 10)
+
+@pet_1 = Pet.create(adoptable: true, age: 1, breed: 'sphynx', name: 'Lucille Bald')
+@pet_2 = Pet.create(adoptable: true, age: 3, breed: 'doberman', name: 'Lobster')
+@pet_3 = Pet.create(adoptable: false, age: 2, breed: 'saint bernard', name: 'Beethoven')
+
+@vet_office_1 = VeterinaryOffice.create(name: 'Special Friends', boarding_services: true, max_patient_capacity: 100)
+@vet_office_2 = VeterinaryOffice.create(name: 'Pet Emergency Room', boarding_services: true, max_patient_capacity: 50)
+@vet_office_3 = VeterinaryOffice.create(name: 'The Country Vet', boarding_services: true, max_patient_capacity: 200)
+
+@vet_1 = Veterinarian.create(name: 'Taylor', review_rating: 10, on_call: true)
+@vet_2 = Veterinarian.create(name: 'Jim', review_rating: 8, on_call: true)
+
+@application = Application.create(name: 'Larry Sanders', address: '22 Shadowbrook Way Mendham, NJ 07945', home_description: "Looks great.")
+@application = Application.create(name: 'Leslie Knope', address: '2358 Highland Ave Pawnee, IN 47906', home_description: "Dog friendly.")
+
